d3ac9179864efe815a3ca483b1adbe25624344a1
DarkoGjichev <darkogicev111@gmail.com>
2025-08-11 16:11:58 +0100

    enables gallery to get a hold of all articles from fetchArticles function

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,3 +1,22 @@
-function Gallery() {}
+import { useEffect, useState } from "react";
+import fetchArticles from "../api/fetch-articles";
+
+function Gallery() {
+  const [articles, setArticals] = useState([]);
+  useEffect(() => {
+    fetchArticles().then(({ articles }) => {
+      setArticals(articles);
+    });
+  }, []);
+
+  return articles.map((article) => {
+    return (
+      <>
+        <h2>{article.title}</h2>
+        <img src={article.article_img_url} alt="" />
+      </>
+    );
+  });
+}
 
 export default Gallery;
