b64bf5b364e6292d7a66c1bf15d1f8941a8f7214
DarkoGjichev <darkogicev111@gmail.com>
2025-08-11 15:36:54 +0100

    add main components for layout of homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Articles from "./pages/Articles";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Articles />} />
+    </Routes>
+  );
 }
 
 export default App;
