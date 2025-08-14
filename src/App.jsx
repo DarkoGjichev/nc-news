7060ffac14e90bf8d944708cbdbddeb4ffca2737
DarkoGjichev <darkogicev111@gmail.com>
2025-08-14 11:23:30 +0100

    add Error route and component for non existing paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import ErrorPage from "./pages/ErrorPage";
 import Footer from "./components/Footer";
 import "./app.css";
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Routes>
+        <Route path="*" element={<ErrorPage />} />
         <Route path="/" element={<Articles />} />
         <Route path="/articles/:article_id" element={<Article />} />
       </Routes>
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+function ErrorPage() {
+  return (
+    <main>
+      <h2>404</h2>
+      <p>Oops! Page not found.</p>
+    </main>
+  );
+}
+
+export default ErrorPage;
