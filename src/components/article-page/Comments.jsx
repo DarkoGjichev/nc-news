1364bd7d67d4f54d9c1594a6cc33c3e55eff0103
DarkoGjichev <darkogicev111@gmail.com>
2025-08-15 16:57:37 +0100

    implement custom hook for api get call for comments

--- src/components/article-page/Comments.jsx
+++ src/components/article-page/Comments.jsx
@@ -2,17 +2,18 @@ import { useState, useEffect } from "react";
 import fetchComments from "../../api/fetch-comments";
 import CommentCard from "./comments-component/CommentCard";
 import CommentForm from "./comments-component/CommentForm";
+import useApiRequest from "../../useApiRequest";
 
 function Comments({ article_id, comment_count }) {
-  const [comments, setComments] = useState([]);
   const [commentInc, setCommentInc] = useState(0);
   const username = "grumpy19";
+  const {
+    isLoading,
+    error,
+    data: { comments = [] },
+    setData: setComments,
+  } = useApiRequest(fetchComments, article_id);
 
-  useEffect(() => {
-    fetchComments(article_id).then(({ comments }) => {
-      setComments(comments);
-    });
-  }, []);
   return (
     <>
       <section className="comments--wrapper">
