ba06c07b4e4966636621bff149ccf506e51aa753
DarkoGjichev <darkogicev111@gmail.com>
2025-08-15 16:54:27 +0100

    implement custom hook for api get call for single article

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import fetchSingleArticle from "../api/fetch-single-article";
 import Comments from "../components/article-page/Comments";
@@ -7,45 +6,27 @@ import Tags from "../components/article-page/Tags";
 import Text from "../components/article-page/Text";
 import ErrorPage from "../pages/ErrorPage";
 import "../components/article-page/article.css";
+import useApiRequest from "../useApiRequest";
 
 function Article() {
   const { article_id } = useParams();
-  const [singleArticle, setSingleArticle] = useState([]);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetchSingleArticle(article_id)
-      .then(({ article }) => {
-        setSingleArticle(article);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, []);
+  const {
+    isLoading,
+    error,
+    data: { article = [] },
+  } = useApiRequest(fetchSingleArticle, article_id);
+
   if (error) {
     return <ErrorPage />;
   }
 
   return (
     <main className="article-container">
-      <img
-        className="article-image"
-        src={singleArticle.article_img_url}
-        alt=""
-      />
-      <Tags
-        author={singleArticle.author}
-        created_at={singleArticle.created_at}
-      />
-      <Text
-        topic={singleArticle.topic}
-        title={singleArticle.title}
-        body={singleArticle.body}
-      />
-      <Votes votes={singleArticle.votes} article_id={article_id} />
-      <Comments
-        article_id={article_id}
-        comment_count={singleArticle.comment_count}
-      />
+      <img className="article-image" src={article.article_img_url} alt="" />
+      <Tags author={article.author} created_at={article.created_at} />
+      <Text topic={article.topic} title={article.title} body={article.body} />
+      <Votes votes={article.votes} article_id={article_id} />
+      <Comments article_id={article_id} comment_count={article.comment_count} />
     </main>
   );
 }
