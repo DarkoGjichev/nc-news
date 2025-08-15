be95f7f7c96c2a5621735ff8c5d051306d5ada4e
DarkoGjichev <darkogicev111@gmail.com>
2025-08-15 16:46:44 +0100

    add custom hook for api call

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,27 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import fetchArticles from "../api/fetch-articles";
 import Toolbar from "../components/articles-page/Toolbar";
 import ArticleCard from "../components/articles-page/ArticleCard";
 import { useSearchParams } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
+import useApiRequest from "../useApiRequest";
 
 function Articles() {
   const [searchParams] = useSearchParams();
   const topic = searchParams.get("topic");
-  const [articles, setArticals] = useState([]);
   const [sortBy, setSortBy] = useState("created_at");
   const [orderBy, setOrderBy] = useState("DESC");
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    setError(null);
-    fetchArticles(topic, sortBy, orderBy)
-      .then(({ articles }) => {
-        setArticals(articles);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, [topic, sortBy, orderBy]);
+  const {
+    isLoading,
+    error,
+    data: { articles = [] },
+    setData: setArticals,
+  } = useApiRequest(fetchArticles, topic, sortBy, orderBy);
+
   if (error) {
     return <ErrorPage />;
   }
---
+++ src/useApiRequest.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function useApiRequest(apiFunction, ...arg) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    apiFunction(...arg)
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [...arg, apiFunction]);
+
+  return { isLoading, error, data, setData };
+}
+
+export default useApiRequest;
