57b812620eaff497f56d005aced36b0db4adf448
DarkoGjichev <darkogicev111@gmail.com>
2025-08-15 11:05:15 +0100

    add error message when trying to post empty comment

--- src/components/article-page/comments-component/CommentForm.jsx
+++ src/components/article-page/comments-component/CommentForm.jsx
@@ -3,10 +3,14 @@ import postComment from "../../../api/post-a-comment";
 
 function CommentForm({ article_id, setComments, setCommentInc, username }) {
   const [commentText, setCommentText] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (commentText.trim() === "") return;
+    if (commentText.trim() === "") {
+      setError("Your comment must contain at least 1 character");
+      return;
+    }
 
     postComment(article_id, username, commentText).then(({ postedComment }) => {
       setComments((prevComments) => [postedComment, ...prevComments]);
@@ -19,8 +23,11 @@ function CommentForm({ article_id, setComments, setCommentInc, username }) {
     <form onSubmit={handleSubmit}>
       <textarea
         value={commentText}
-        onChange={(event) => setCommentText(event.target.value)}
-        placeholder="Add a comment..."
+        onChange={(event) => {
+          setCommentText(event.target.value);
+          setError("");
+        }}
+        placeholder={error ? error : "Add a comment..."}
       />
       <button type="submit">Submit</button>
     </form>
